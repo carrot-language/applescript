d18487d84910ae99e27a38aae1f3f71165fca54c
William McGonagle <wpmcgonagle@gmail.com>
2022-02-06 22:17:30 -0500

    Created Src File

---
+++ src/index.js
@@ -0,0 +1,29 @@
+function getEmoji() {
+
+    return '🍏';
+
+}
+
+function getName() {
+
+    return 'AppleScript';
+
+}
+
+function getStatus() {
+
+    // Needs Work
+    // In-Development
+    // Working
+    // Done
+    // Deprecated
+
+    return 0;
+
+}
+
+module.exports = {
+    getName,
+    getStatus,
+    getEmoji
+};
